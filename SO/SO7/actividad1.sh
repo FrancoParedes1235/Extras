2591ff696e7bafeb0e63171d03a988b698280318
FrancoParedesZamora <111542808+FrancoParedes1235@users.noreply.github.com>
2024-05-13 16:17:11 -0500

    Update actividad1.sh

--- SO/SO7/actividad1.sh
+++ SO/SO7/actividad1.sh
@@ -9,16 +9,15 @@ done
 
 echo "${arreglos[@]}"
 
-for i in "${arreglos[@]}"; do
-    echo "${arreglos[i]}"
-    #echo "$i"
+for elem in "${arreglos[@]}"; do
+    echo "$elem"
 done
 
-echo "El número de elementosdel arreglo es: (Obtén el número de elementos del arreglo): ${#arreglos[@]}"
+echo "El número de elementos del arreglo es: ${#arreglos[@]}"
 
-echo "Ingreso el numero de arreglo a borrar"
+echo "Ingrese el índice del elemento a borrar"
 read borrar
 unset arreglos[$borrar]
 
-echo "${arreglo[@]}"
-echo "${#arreglo[@]}"-
+echo "${arreglos[@]}"
+echo "El número de elementos del arreglo es: ${#arreglos[@]}"
